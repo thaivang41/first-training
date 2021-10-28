84a2cc8df89970bc7e00513081c4506eef6cbcd2
thaivang41 <thai.c.vang@gmail.com>
2021-10-28 16:38:30 -0500

    updated js

---
+++ index.js
@@ -0,0 +1,39 @@
+//document.getElementById("count-el").innerText
+
+//let firstBatch = 5
+//let secondBatch = 7
+
+//let count = firstBatch + secondBatch
+
+//console.log(count)
+
+//let myAge = 41
+
+// //console.log(myAge)
+
+//let myAge = 41
+//let humanDogRatio = 7
+
+//let myDogAge = myAge * humanDogRatio
+
+//console.log (myDogAge)
+
+
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+// Setting up the race
+
+countdown()
+
+//Go!
+//Players are running the race
+//Race is finished!
+
+//Get ready for a new race
